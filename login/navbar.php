24da774352163e42fedf4bbb575feb99631ae8c2
manogta1 <manuel@mprinz.net>
2024-11-08 22:22:29 +0100

    Add files via upload

---
+++ login/navbar.php
@@ -0,0 +1,9 @@
+<!-- navbar.php -->
+<nav>
+    <ul>
+        <li><a href="index.php">Startseite</a></li>
+        <li><a href="profile.php">Profil</a></li>
+        <li><a href="profile_settings.php">Profil-Einstellungen</a></li>
+        <li><a href="logout.php">Abmelden</a></li>
+    </ul>
+</nav>
